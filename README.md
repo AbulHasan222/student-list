d7f1436e03b7b3a1f8ec3f5128219bb929c9056f
Md. Abul Hasan <52633243+AbulHasan222@users.noreply.github.com>
2021-03-18 19:31:53 +0600

    Student Crud in MVC
    All the replacement are written in README.md file.

---
+++ README.md
@@ -0,0 +1,7 @@
+# student-list
+Take a ASP.Net web project>MVC
+Connect Database >EF Designer From Database
+DB Table: dbl_student: ID int primary key, FirstName, LastName, Email, Mobile, Address
+Replace>View>Shared>_Layout.cshtml
+Replace>View>Student>Student.cshtml and StudentList.cshtml
+Replace>Models>dbl_student.cs
---
+++ StudentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentDataCRUD_MVC.Models;
+using System.Data;
+
+namespace StudentDataCRUD_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        PracticeDBEntities dbObj = new PracticeDBEntities();
+        // GET: Student
+        public ActionResult Student(dbl_student student)
+        {
+               return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(dbl_student model)
+        {
+            if (ModelState.IsValid)
+            {
+                dbl_student obj = new dbl_student();
+                obj.ID = model.ID;
+                obj.FirstName = model.FirstName;
+                obj.LastName = model.LastName;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Address = model.Address;
+
+                if(model.ID == 0)
+                {
+                    dbObj.dbl_student.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+            }
+            ModelState.Clear();
+
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var studentList = dbObj.dbl_student.ToList();
+
+            return View(studentList);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.dbl_student.Where(x => x.ID == id).First();
+            dbObj.dbl_student.Remove(res);
+            dbObj.SaveChanges();
+
+            var stList = dbObj.dbl_student.ToList();
+            return View("StudentList", stList);
+        }
+
+    }
+}
